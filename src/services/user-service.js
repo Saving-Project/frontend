be9d5bf58079a2898168c428e543f1981fcc6f61
SebastianSamboni <sebas_18_08@hotmail.com>
2024-05-28 15:11:56 -0500

    Create user services file to consume api

---
+++ src/services/user-service.js
@@ -0,0 +1,5 @@
+import axios from './axios'
+
+export const registerRequest = user => axios.post('/user', user)
+export const loginRequest = user => axios.post('/login', user)
+export const getInfoRequest = user => axios.get('/user')
