34701d1f3589dc75e443e48976cc08573aeb21a8
SebastianSamboni <sebas_18_08@hotmail.com>
2024-06-02 20:14:06 -0500

    Change home interface and implement reset button

--- src/components/PlanCard.jsx
+++ src/components/PlanCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const PlanCard = () => {
+const PlanCard = ({ description, value, onOpen }) => {
     return (
         <div className='bg-white p-4 rounded-lg shadow-lg w-full flex flex-col justify-between'>
             <div className='flex justify-between items-center mb-4'>
-                <h2 className='text-xl font-bold'>Descripción</h2>
-                <span className='text-lg'>$0</span>
+                <h2 className='text-xl font-bold'>{description}</h2>
+                <span className='text-lg'>${value}</span>
             </div>
-            <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 self-center'>
+            <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 self-center'
+            onClick={onOpen}>
                 ABRIR
             </button>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Saving from '../components/Saving'
 import { LuLogOut } from 'react-icons/lu'
+import PlanCard from '../components/PlanCard'
 
 const Home = () => {
+    const [selectedPlan, setSelectedPlan] = useState(null)
+
+    const plans = [
+        { id: 1, description: 'Plan A', value: 500 },
+        { id: 2, description: 'Plan B', value: 1000 }
+    ]
+
+    const handleOpenPlan = id => {
+        setSelectedPlan(id)
+    }
+    const handleBack = () => {
+        setSelectedPlan(null)
+    }
+
     return (
-        <div className='flex flex-col min-h-screen'>
+        <div className='flex flex-col min-h-screen relative'>
             <button className='absolute top-4 right-4 p-2 rounded-full bg-gray-200 hover:bg-gray-300'>
                 <LuLogOut className='text-xl'/>
             </button>
-            <Saving />
+
+            {selectedPlan === null ? (
+                <div className='container mx-auto py-4 px-32 flex flex-col items-center'>
+                    <h1 className='text-3xl font-bold mb-6'>Bienvenido, Fulano</h1>
+                    <h3 className='text-2xl mb-6'>Tus planes de ahorro activos</h3>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-4 w-full'>
+                        {plans.map(plan => (
+                            <PlanCard key={plan.id}
+                                description={plan.description}
+                                value={plan.value}
+                                onOpen={() => handleOpenPlan(plan.id)}
+                            />
+                        ))}
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <button className='absolute top-4 left-4 p-2 rounded-full bg-gray-200 hover:bg-gray-300'
+                        onClick={handleBack}
+                    >
+                        Volver
+                    </button>
+                    <Saving />    
+                </div>
+            )}
         </div>
     )
 }
