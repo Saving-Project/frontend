dfa7aff2e735cf3a70ed7261da97e8b9e7a3b7f9
SebastianSamboni <sebas_18_08@hotmail.com>
2024-06-05 12:21:05 -0500

    Implement button to open modal with plan creation window

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import Saving from '../components/Saving'
 import { LuLogOut } from 'react-icons/lu'
 import PlanCard from '../components/PlanCard'
+import Modal from '../components/Modal'
 
 const Home = () => {
     const [selectedPlan, setSelectedPlan] = useState(null)
+    const [isOpen, setIsOpen] = useState(false)
 
     const plans = [
         { id: 1, description: 'Plan A', value: 500 },
@@ -17,6 +19,12 @@ const Home = () => {
     const handleBack = () => {
         setSelectedPlan(null)
     }
+    const handleOpenModal = () => {
+        setIsOpen(true)
+    }
+    const handleCloseModal = () => {
+        setIsOpen(false)
+    }
 
     return (
         <div className='flex flex-col min-h-screen relative'>
@@ -28,6 +36,10 @@ const Home = () => {
                 <div className='container mx-auto py-4 px-32 flex flex-col items-center'>
                     <h1 className='text-3xl font-bold mb-6'>Bienvenido, Fulano</h1>
                     <h3 className='text-2xl mb-6'>Tus planes de ahorro activos</h3>
+                    <button onClick={handleOpenModal}
+                        className='mb-6 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700'
+                    >
+                        Crear Nuevo Plan</button>
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-4 w-full'>
                         {plans.map(plan => (
                             <PlanCard key={plan.id}
@@ -37,6 +49,7 @@ const Home = () => {
                             />
                         ))}
                     </div>
+                    <Modal open={isOpen} onClose={handleCloseModal}/>
                 </div>
             ) : (
                 <div>
