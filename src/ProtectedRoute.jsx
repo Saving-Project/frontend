d37e236dbc63f319d00b8e0a0caa87685fdf80bc
SebastianSamboni <sebas_18_08@hotmail.com>
2024-06-06 16:36:13 -0500

    Implement protection to routes with authentication

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from './context/AuthContext'
+import { Navigate } from 'react-router-dom'
 
 const ProtectedRoute = () => {
-    return (
-        <div>
-        
-        </div>
-    )
+    const { isAuthenticated, loading } = useContext(AuthContext)
+
+    if (loading) return <h1>
+        Loading...
+    </h1>
+    if (!loading && !isAuthenticated) return <Navigate to='/' replace />
+
+    return <Outlet />
 }
 
 export default ProtectedRoute
