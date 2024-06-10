003c9f9279404a05a7a5b56ac78098ece0d8c1ee
SebastianSamboni <sebas_18_08@hotmail.com>
2024-06-10 14:29:58 -0500

    Create and implement on components get plan by id method

--- src/components/Saving.jsx
+++ src/components/Saving.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import DayCard from '../components/DayCard'
 import InfoData from '../components/InfoData'
-import { LuLogOut } from 'react-icons/lu'
+import { PlanContext } from '../context/PlanContext'
+import { AuthContext } from '../context/AuthContext'
 
-const Saving = () => {
+const Saving = ({ idPlan }) => {
+    const { plan, planErrors, fetchPlanInfo } = useContext(PlanContext)
+    const { isAuthenticated } = useContext(AuthContext)
+
+    useEffect(() => {
+        fetchPlanInfo(idPlan)
+    }, [isAuthenticated, idPlan])
+
+    if (!plan) {
+        return <div>Loading...</div>
+    }
+
     const cards = []
     for (let i = 1; i <= 200; i++) {
         cards.push({number: i, price: i * 50})
     }
     return (
         <div className="container mx-auto py-4 px-32">
-            <InfoData />
+            <InfoData description={plan.description} value={plan.total_saving} />
             <div className='h-[70vh] flex-1 overflow-y-auto'>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                     {cards.map(card => (
